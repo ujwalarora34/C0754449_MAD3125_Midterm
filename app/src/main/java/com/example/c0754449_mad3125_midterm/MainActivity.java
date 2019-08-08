62dbc277cb259914c3c337344b0fc2af98a5d582
MacStudent <macstudent@130-IMAC-32.local>
2019-08-08 18:47:23 -0400

    Initial commit

---
+++ app/src/main/java/com/example/c0754449_mad3125_midterm/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.c0754449_mad3125_midterm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
